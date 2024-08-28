bf43239601f7f373691661c5aa349e9db3e3cd61
adam <adam.straube@okta.com>
2024-08-28 16:53:38 +1000

    Fix for auth0 Nextjs instancing

--- app/api/auth/[auth0]/route.js
+++ app/api/auth/[auth0]/route.js
@@ -1,6 +1,6 @@
 import auth0 from '../../../../lib/auth0';
 
-const handler = auth0.handleAuth();
+const handler = auth0().handleAuth();
 
 // For Back-Channel Logout you need to export a GET and a POST handler.
 export { handler as GET, handler as POST };
